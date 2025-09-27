86cb37b13c80249f72238de3083c01cd576b30f5
Ubuntu <ubuntu@ip-172-31-42-16.ap-south-1.compute.internal>
2025-09-27 18:22:01 +0000

    terraform version.tf

---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-1"
+}
+
---
+++ version.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-dev-1"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    use_lockfile    = true
+    encrypt        = true
+  }
+  
+  
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
