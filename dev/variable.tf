63b20e3ce7a7a122584ef62e6707b44be33e7117
vinay singh <vinaysinghhata5@gmail.com>
2025-09-27 18:36:46 +0000

    vpc creation added

--- dev/variable.tf
+++ dev/variable.tf
@@ -10,4 +10,8 @@ variable "project_name" {
   default     = "prod-vpc-dev"
 }
 
-
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
