ec0e3b616124a07b1a62140f43d2ed52e7e9f5fb
vinay singh <vinaysinghhata5@gmail.com>
2025-10-01 06:13:25 +0000

    addons add

--- dev/eks/addons.tf
+++ dev/eks/addons.tf
@@ -1,13 +1,3 @@
-#create addons for cluster
-resource "aws_eks_addon" "vpc_cni" {
-  addon_name       = "vpc-cni"
-  cluster_name     = aws_eks_cluster.eks_cluster.name
-  addon_version    = "v1.19.0-eksbuild.1"
-    resolve_conflicts = "OVERWRITE"
-    service_account_role_arn = aws_iam_role.eks_addon_role.arn 
-    depends_on = [aws_eks_cluster.eks_cluster]
-}
-
 resource "aws_iam_role" "eks_addon_role" {
   name = "${var.project_name}-eks-addon-role"
   assume_role_policy = jsonencode({
@@ -27,27 +17,11 @@ resource "aws_iam_role_policy_attachment" "eks_AmazonEKS_CNI_Policy_addon" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.eks_addon_role.name
 }
-resource "aws_eks_addon" "kube_proxy" {
-  addon_name       = "kube-proxy"
-  cluster_name     = aws_eks_cluster.eks_cluster.name
-  addon_version    = "v1.19.0-eksbuild.1"
-    resolve_conflicts = "OVERWRITE"
-    service_account_role_arn = aws_iam_role.eks_addon_role.arn
-    depends_on = [aws_eks_cluster.eks_cluster]
-}
-resource "aws_eks_addon" "core_dns" {
-  addon_name       = "coredns"
-  cluster_name     = aws_eks_cluster.eks_cluster.name
-  addon_version    = "v1.8.0-eksbuild.1"
-    resolve_conflicts = "OVERWRITE"
-    service_account_role_arn = aws_iam_role.eks_addon_role.arn
-    depends_on = [aws_eks_cluster.eks_cluster]
-}
 
 resource "aws_eks_addon" "aws_ebs_csi_driver" {
   addon_name       = "aws-ebs-csi-driver"
   cluster_name     = aws_eks_cluster.eks_cluster.name
-  addon_version    = "v1.4.0-eksbuild.1"
+  addon_version    = "v1.48.0-eksbuild.2"
     resolve_conflicts = "OVERWRITE"
     service_account_role_arn = aws_iam_role.eks_addon_role.arn 
     depends_on = [aws_eks_cluster.eks_cluster]
